02d90d61d7620e292fd41b5de1f0a22de9a42b8f
Tristan Ravitch <travitch@cs.wisc.edu>
2010-03-05 10:23:57 -0600

    Fix the masking of unused bits

--- Data/BitVector.hs
+++ Data/BitVector.hs
@@ -9,7 +9,7 @@ module Data.BitVector ( BitVector(..)
 import Data.Bits
 import qualified Data.ByteString as BS
 import Data.ByteString.Num
-import Data.List (unfoldr, foldl', groupBy, sort)
+import Data.List (unfoldr, foldl', groupBy, sort, replicate)
 import Data.Word
 
 import Debug.Trace
@@ -42,11 +42,9 @@ instance Bits BitVector where
   bitSize (BitVector v u) = 8 * (length $ BS.unpack v) - u
   isSigned bv = False
 
-bitOp op (BitVector v1 un1) (BitVector v2 un2) = BitVector (op v1 v2) (min un1 un2)
-  -- if (BS.length v1) == (BS.length v2) && (un1 == un2)
-  --    then
-  --    else error "Bytestrings not equal size"
-  -- where newV = v1 `op` v2
+bitOp op (BitVector v1 u1) (BitVector v2 u2) = BitVector newV newU
+  where newV = v1 `op` v2
+        newU = if (BS.length v1) > (BS.length v2) then u1 else u2
 
 
 internalShow bv0 = concatMap byteToString $ BS.unpack bv
@@ -54,8 +52,9 @@ internalShow bv0 = concatMap byteToString $ BS.unpack bv
 byteToString byte = concat $ map f [0..7]
   where f i = if testBit byte i then "1" else "0"
 
-indexGroupToByte baseByte thisGroup = foldl' complementBit baseByte modGroup
+indexGroupToByte baseByte thisGroup = byte  `debug` (" igb: " ++ show modGroup ++ " " ++ show byte)
   where modGroup = map (\x -> mod x 8) thisGroup
+        byte = foldl' complementBit baseByte modGroup
 
 groupBitsByByte bitsSet = groupBy bitsGrouped sortedIndices
   where sortedIndices = sort bitsSet
@@ -64,25 +63,37 @@ groupBitsByByte bitsSet = groupBy bitsGrouped sortedIndices
 
 makeBitVector nBits initVal bitsSet =
   if nBits >= 0
-    then BitVector bv (8-leftoverBits) -- `debug` (" rev " ++ show bv)
+    then BitVector bv (8-leftoverBits) `debug` ( " groups: " ++ show byteGroups ) -- `debug` (" rev " ++ show bv)
     else error "BitVectors cannot have negative length"
   where baseByte = if initVal then 0 else (-1) :: Word8
         (fullBytes, leftoverBits) = nBits `divMod` 8
         totalBytes = fullBytes + signum leftoverBits
 
         byteGroups = groupBitsByByte bitsSet
-        bv = BS.pack $ reverse $ unfoldr byteAccum (0, byteGroups)
+        bv = BS.pack $ byteList `debug` ( "byteList = " ++ show byteList )
         indexOfGroup (firstIdx:rest) = firstIdx `div` 8
         byteAccum (idx, []) | idx < totalBytes = Just (baseByte, (idx+1, []))
         byteAccum (idx, grp:rest) = if idx == indexOfGroup grp
                                       then Just (indexGroupToByte baseByte grp, (idx+1, rest))
                                       else Just (baseByte, (idx+1, grp:rest))
         byteAccum (idx, _) | idx == totalBytes = Nothing
+        byteList = unfoldr byteAccum (0, byteGroups)
 
-maskUnusedBits (BitVector v unusedBits) = v .&. bitmask'
-  where bitmask = fromInteger $ toInteger $ round (2^unusedBits - 1)
-        bitmask' = complement bitmask
+maskUnusedBits bv@(BitVector v unusedBits) =
+  if unusedBits == 0
+     then v
+     else v .&. bitmask `debug` (" mask " ++ show bitmask)
+  where bitmask = BS.pack $ reverse $ (indexGroupToByte 0 [0..(8-unusedBits-1)]) : baseBS
+        baseBS = replicate (BS.length v - 1) (-1)
 
 -- Need to mask out the high unused bits
--- toByteString :: BitVector -> BS.ByteString
 toByteString bv = maskUnusedBits bv
+
+main = do
+  let bvs = [ makeBitVector 10 True [2, 5]
+            , makeBitVector 10 True [0]
+            , makeBitVector 10 True [9]
+            , (makeBitVector 10 True [2, 5]) .|. (makeBitVector 10 True [9])
+            , complement $ (makeBitVector 10 True [2, 5]) .|. (makeBitVector 10 True [9])
+              ]
+  mapM_ (\x -> putStrLn $ "\n" ++ show x) bvs
